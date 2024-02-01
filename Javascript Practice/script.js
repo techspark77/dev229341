66c61670310c7c25fc8ed20a38a0d602b62b0cf7
seanfancy <68828262+techspark77@users.noreply.github.com>
2024-02-01 01:26:08 -0500

    Add Variable and array object in script.js

--- Javascript Practice/script.js
+++ Javascript Practice/script.js
@@ -1,2 +1,43 @@
 console.log('This is my first javascript file!\nMy Name is Bao')
 // console.log('Baobao')
+
+// Below is a variable object
+let member = {
+    Name : "Niamul",
+    Age : 20,
+    City : "Bristol",
+    State : "CT",
+    Country: "USA"
+}
+
+
+//Below is a array object
+const members = [
+    {
+        Name: "Niamul",
+        Age: 36,
+        Country: "USA"
+    },
+    {
+        Name: "Farah",
+        Age: 31,
+        Country: "Kuwait"
+    },
+    {
+        Name: "Shihab",
+        Age: 333,
+        Country: "UAE"
+    }
+]
+
+console.log(`${member.Name} is a member of the community`);
+
+console.log(`${members[1].Country} is a middle eastern country`);
+
+const a = () => {
+
+    console.log("blabla");
+
+};
+
+a();
